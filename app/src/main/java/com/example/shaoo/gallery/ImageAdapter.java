c8e1e73e4bfec07bb797e7f84834f7ca9ed88843
Shaoor Munir <shaoormunir@outlook.com>
2016-09-30 16:57:11 +0500

    Completed image adapter class

--- app/src/main/java/com/example/shaoo/gallery/ImageAdapter.java
+++ app/src/main/java/com/example/shaoo/gallery/ImageAdapter.java
@@ -1,12 +1,21 @@
 package com.example.shaoo.gallery;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import static android.R.id.list;
 
 public class ImageAdapter extends BaseAdapter {
 
@@ -20,7 +29,11 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        load_images();
+        try {
+            load_images();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return assetsImages.length;
     }
 
@@ -54,8 +67,16 @@ public class ImageAdapter extends BaseAdapter {
 
     }
 
-    private void load_images(){
+    private void load_images() throws IOException {
+        AssetManager mgr = mContext.getAssets();
 
+        String[] imagePaths = mgr.list("");
+        assetsImages= new Bitmap[imagePaths.length];
+
+        for(int i=0;i<imagePaths.length;i++)
+        {
+            assetsImages[i] = BitmapFactory.decodeStream(mgr.open(imagePaths[i]));
+        }
 
     }
 }
